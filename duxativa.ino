a0ac991e77914c587296a80d5f061c1e6cd131ca
Iuseh <eunaif97@gmail.com>
2017-11-02 09:35:22 +0100

    mi primercommit

---
+++ duxativa.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  // put your main code here, to run repeatedly:
+
+}
